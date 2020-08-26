c79f9f618fd94cb65882427434f8a93dc0b03175
Paulus Schoutsen <balloob@gmail.com>
2020-08-26 15:58:46 +0000

    Version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
 
-long_description = open('README.md').read()
+long_description = open("README.md").read()
 
 setup(
-    name='aioshelly',
-    version='0.1.2',
-    license='Apache License 2.0',
-    url='https://github.com/home-assistant-libs/aioshelly',
-    author='Paulus Schoutsen',
-    author_email='paulus@home-assistant.io',
-    description='Python module to talk to Philips Hue.',
+    name="aioshelly",
+    version="0.2.0",
+    license="Apache License 2.0",
+    url="https://github.com/home-assistant-libs/aioshelly",
+    author="Paulus Schoutsen",
+    author_email="paulus@home-assistant.io",
+    description="Python module to talk to Philips Hue.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=['aioshelly'],
+    long_description_content_type="text/markdown",
+    packages=["aioshelly"],
     zip_safe=True,
-    platforms='any',
-    install_requires=list(val.strip() for val in open('requirements.txt')),
+    platforms="any",
+    install_requires=list(val.strip() for val in open("requirements.txt")),
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
