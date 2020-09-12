85616adef0cfa6e03307b94ed14b9dd6fafd85b6
chemelli74 <simone.chemelli@gmail.com>
2020-09-12 16:03:26 +0200

    Add REST call to /status neded for RSSI, Cloud, Uptime sensors

--- aioshelly/__init__.py
+++ aioshelly/__init__.py
@@ -93,6 +93,7 @@ class Device:
         self.s = None
         self._settings = None
         self.shelly = None
+        self._status = None
 
     @classmethod
     async def create(
@@ -145,6 +146,7 @@ class Device:
 
         if self.auth or not self.shelly["auth"]:
             self._settings = await self.http_request("get", "settings")
+            self._status = await self.http_request("get", "status")
 
     async def update(self):
         self.s = {info[1]: info[2] for info in (await self.coap_request("s"))["G"]}
@@ -185,6 +187,12 @@ class Device:
 
         return self._settings
 
+    @property
+    def status(self):
+        if self._status is None:
+            raise AuthRequired
+
+        return self._status
 
 class Block:
     TYPES = {}
