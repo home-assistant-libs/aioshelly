760cd947a24f66fbf4026d4c9dd7382052bc83fe
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2020-10-02 14:43:39 +0200

    Version bump to 0.3.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open("README.md").read()
 
 setup(
     name="aioshelly",
-    version="0.3.3",
+    version="0.3.4",
     license="Apache License 2.0",
     url="https://github.com/home-assistant-libs/aioshelly",
     author="Paulus Schoutsen",
