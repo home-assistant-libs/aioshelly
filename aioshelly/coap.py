0b8c6e329cc5332967b2c2001c1433f045c17ff9
Paulus Schoutsen <balloob@gmail.com>
2020-10-23 11:10:19 +0000

    Add support for discovery

--- aioshelly/coap.py
+++ aioshelly/coap.py
@@ -8,6 +8,27 @@ import struct
 BUFFER = 2048
 
 
+class CoapMessage:
+    def __init__(self, sender_addr, payload: bytes):
+        self.ip = sender_addr[0]
+        self.port = sender_addr[1]
+        header, payload = payload.split(b"\xff", 1)
+        self.header = header
+        self.payload = json.loads(payload.decode())
+
+
+class DiscoveryProtocol(asyncio.DatagramProtocol):
+
+    def __init__(self, msg_received) -> None:
+        self.msg_received = msg_received
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        self.msg_received(CoapMessage(addr, data))
+
+
 def socket_init():
     """Init UDP socket to send/receive data with Shelly devices."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -22,8 +43,15 @@ def socket_init():
 class COAP:
     """Initialize the COAP manager."""
 
-    def __init__(self):
+    def __init__(self, status_update_received=None):
+        self.sock = None
+        self.status_update_received = status_update_received
+
+    async def initialize(self):
+        loop = asyncio.get_running_loop()
         self.sock = socket_init()
+        if self.status_update_received:
+            await loop.create_datagram_endpoint(lambda: DiscoveryProtocol(self.status_update_received), sock=self.sock)
 
     async def request(self, ip: str, path: str):
         """Device CoAP request."""
@@ -33,21 +61,28 @@ class COAP:
         )
         self.sock.sendto(msg, (ip, 5683))
         response = await loop.sock_recv(self.sock, BUFFER)
-        if path == "d":
-            header = b'"blk":'
-            prefix = '{"blk":'
-        else:
-            header = b'"G":'
-            prefix = '{"G":'
-        payload_bytes = response.split(header)[1]
-        payload = prefix + payload_bytes.decode()
-        return json.loads(payload)
+
+        return CoapMessage((ip, 5683), response).payload
 
     def close(self):
         self.sock.close()
 
     async def __aenter__(self):
+        await self.initialize()
         return self
 
     async def __aexit__(self, type, value, traceback):
         self.close()
+
+
+async def discovery_dump():
+    async with COAP(lambda msg: print(msg.ip, msg.payload)):
+        while True:
+            await asyncio.sleep(.1)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(discovery_dump())
+    except KeyboardInterrupt:
+        pass
