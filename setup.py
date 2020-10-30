9cd8faff10f55b08f0a2429aa805cea7a488ba49
Maciej Bieniek <bieniu@users.noreply.github.com>
2020-10-30 08:58:50 +0100

    Bump version to 0.5.0 (#40)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open("README.md").read()
 
 setup(
     name="aioshelly",
-    version="0.4.0",
+    version="0.5.0",
     license="Apache License 2.0",
     url="https://github.com/home-assistant-libs/aioshelly",
     author="Paulus Schoutsen",
