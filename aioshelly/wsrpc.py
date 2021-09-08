8f465f307541b86b344c7cfa3be13b4f56b2fe30
thecode <levyshay1@gmail.com>
2021-09-08 19:19:22 +0000

    Add heartbeat to wsrpc

--- aioshelly/wsrpc.py
+++ aioshelly/wsrpc.py
@@ -7,9 +7,9 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Any, Dict
 
-import aiohttp
 from aiohttp import ClientWebSocketResponse, client_exceptions
 
+from .const import WS_HEARTBEAT_SEC
 from .exceptions import (
     CannotConnect,
     InvalidMessage,
@@ -52,14 +52,14 @@ class WsRPC:
 
     def __init__(self, ip_address: str, on_notification):
         """Initialize WsRPC class."""
-        self.ip_address = ip_address
-        self.on_notification = on_notification
-        self.rx_task = None
-        self.websocket: ClientWebSocketResponse | None = None
-        self.calls: Dict[str, str] = {}
+        self._ip_address = ip_address
+        self._on_notification = on_notification
+        self._rx_task = None
+        self._websocket: ClientWebSocketResponse | None = None
+        self._calls: Dict[str, str] = {}
         self._call_id = 1
-        self.src = f"aios-{id(self)}"
-        self.dst = None
+        self._src = f"aios-{id(self)}"
+        self._dst = None
 
     @property
     def _next_id(self):
@@ -69,52 +69,52 @@ class WsRPC:
     async def connect(self, aiohttp_session):
         """Connect to device."""
         if self.connected:
-            _LOGGER.debug("%s already connected", self.ip_address)
+            _LOGGER.debug("%s already connected", self._ip_address)
             return
 
-        _LOGGER.debug("Trying to connect to device at %s", self.ip_address)
+        _LOGGER.debug("Trying to connect to device at %s", self._ip_address)
         try:
-            self.websocket = await aiohttp_session.ws_connect(
-                f"http://{self.ip_address}/rpc"
+            self._websocket = await aiohttp_session.ws_connect(
+                f"http://{self._ip_address}/rpc", heartbeat=WS_HEARTBEAT_SEC
             )
         except (
             client_exceptions.WSServerHandshakeError,
             client_exceptions.ClientError,
         ) as err:
-            raise CannotConnect(f"Error connecting to {self.ip_address}") from err
+            raise CannotConnect(f"Error connecting to {self._ip_address}") from err
 
-        self.rx_task = asyncio.create_task(self._rx_msgs())
+        self._rx_task = asyncio.create_task(self._rx_msgs())
 
-        _LOGGER.info("Connected to %s", self.ip_address)
+        _LOGGER.info("Connected to %s", self._ip_address)
 
     async def disconnect(self):
         """Disconnect all sessions."""
-        if self.websocket is None:
+        if self._websocket is None:
             return
 
-        websocket, self.websocket = self.websocket, None
+        websocket, self._websocket = self._websocket, None
         await websocket.close()
 
-        for call_item in self.calls.items():
+        for call_item in self._calls.items():
             call_item.future.cancel()
 
-        self.calls = {}
-        self.rx_task = None
+        self._calls = {}
+        self._rx_task = None
 
     async def _handle_call(self, frame_id):
-        await self.websocket.send_json(
+        await self._websocket.send_json(
             {
                 "id": frame_id,
-                "src": self.src,
+                "src": self._src,
                 "error": {"code": 500, "message": "Not Implemented"},
             }
         )
 
     async def _handle_frame(self, frame):
         if peer_src := frame.get("src", None):
-            if self.dst is not None and peer_src != self.dst:
-                _LOGGER.warning("Remote src changed: %s -> %s", self.dst, peer_src)
-            self.dst = peer_src
+            if self._dst is not None and peer_src != self._dst:
+                _LOGGER.warning("Remote src changed: %s -> %s", self._dst, peer_src)
+            self._dst = peer_src
 
         frame_id = frame.get("id", None)
         method = frame.get("method", None)
@@ -129,27 +129,23 @@ class WsRPC:
             else:
                 # this is a notification
                 _LOGGER.debug("Notification: %s %s", method, params)
-                await self.on_notification(method, params)
+                await self._on_notification(method, params)
 
         elif frame_id:
             # looks like a response
-            if frame_id not in self.calls:
+            if frame_id not in self._calls:
                 _LOGGER.warning("Response for an unknown request id: %s", frame_id)
                 return
 
-            call = self.calls.pop(frame_id)
+            call = self._calls.pop(frame_id)
             call.resolve.set_result(frame)
 
         else:
             _LOGGER.warning("Invalid frame: %s", frame)
 
     async def _rx_msgs(self):
-        async for msg in self.websocket:
-            _LOGGER.debug("Receive %s: %s", msg.type, msg.data)
-            if msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.ERROR):
-                _LOGGER.warning("%s disconnected", self)
-                self.websocket = None
-                return
+        async for msg in self._websocket:
+            _LOGGER.debug("Received Message: Type: %s,  Data: %s", msg.type, msg.data)
 
             try:
                 frame = msg.json()
@@ -158,24 +154,24 @@ class WsRPC:
             else:
                 await self._handle_frame(frame)
 
-        error = str(self.websocket.exception()) if self.websocket else "Disconnected"
+        error = str(self._websocket.exception()) if self._websocket else "Disconnected"
         _LOGGER.debug("Websocket error: %s", error)
-        await self.on_notification("WebSocketClosed", {"error": error})
+        await self._on_notification("WebSocketClosed", {"error": error})
 
     @property
     def connected(self) -> bool:
         """Return if we're currently connected."""
-        return self.websocket is not None and not self.websocket.closed
+        return self._websocket is not None and not self._websocket.closed
 
     async def call(self, method, params=None, timeout=10):
         """Websocket RPC call."""
         call = RPCCall(self._next_id, method, params)
         call.resolve = asyncio.Future()
-        call.src = self.src
-        call.dst = self.dst
+        call.src = self._src
+        call.dst = self._dst
 
-        self.calls[call.call_id] = call
-        await self.websocket.send_json(call.request_frame)
+        self._calls[call.call_id] = call
+        await self._websocket.send_json(call.request_frame)
         call.sent_at = datetime.utcnow()
 
         try:
