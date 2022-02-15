1eff32b752ab1db73a284b87e950b7b98d650857
Reese Wilson <3253971+figadore@users.noreply.github.com>
2022-02-15 11:55:07 -0800

    fixes for pylint, black, mypy, etc

--- aioshelly/wsrpc.py
+++ aioshelly/wsrpc.py
@@ -42,16 +42,23 @@ class RPCCall:
     """RPCCall class."""
 
     def __init__(
-        self, call_id: int, method: str, params: dict[str, Any], auth: dict[str, Any] | None, route: RouteData
+        self,
+        call_id: int,
+        method: str,
+        params: dict[str, Any] | None,
+        route: RouteData,
     ):
         """Initialize RPC class."""
         self.call_id = call_id
-        self.params = params
-        self.auth = auth
         self.method = method
+        self.params = params
         self.src = route.src
         self.dst = route.dst
         self.resolve: asyncio.Future = asyncio.Future()
+        self.auth = {}
+        if params and "auth" in params:
+            self.auth = params["auth"]
+            del params["auth"]
 
     @property
     def request_frame(self) -> dict[str, Any]:
@@ -69,8 +76,12 @@ class RPCCall:
         return msg
 
 
-def hex_hash(message):
-    return hashlib.sha256(message.encode('utf-8')).hexdigest()
+def hex_hash(message: str) -> str:
+    """
+    Get hex representation of sha256 hash of string
+    """
+    return hashlib.sha256(message.encode("utf-8")).hexdigest()
+
 
 class WsRPC:
     """WsRPC class."""
@@ -78,7 +89,7 @@ class WsRPC:
     def __init__(self, ip_address: str, on_notification: Callable) -> None:
         """Initialize WsRPC class."""
         self._ip_address = ip_address
-        self._auth = {}
+        self._auth: dict[str, Any] = {}
         self._on_notification = on_notification
         self._rx_task: tasks.Task[None] | None = None
         self._client: ClientWebSocketResponse | None = None
@@ -91,7 +102,11 @@ class WsRPC:
         self._call_id += 1
         return self._call_id
 
-    async def connect(self, aiohttp_session: aiohttp.ClientSession, options: ConnectionOptions | None = None) -> None:
+    async def connect(
+        self,
+        aiohttp_session: aiohttp.ClientSession,
+        options: ConnectionOptions | None = None,
+    ) -> None:
         """Connect to device."""
         if self.connected:
             raise RuntimeError("Already connected")
@@ -114,37 +129,40 @@ class WsRPC:
         # https://shelly-api-docs.shelly.cloud/gen2/Overview/CommonDeviceTraits/#authentication-over-websocket
         if options and options.password:
             try:
-                result = await self.call("Sys.GetStatus", {})
+                # First API call should fail with 401 if auth enabled
+                await self.call("Sys.GetStatus", {})
             except JSONRPCError as err:
-                if err.code == 401:
-                    response_message = json.loads(err.message)
-
-                    username = "admin" #always
-                    ha1 = hex_hash(f"{username}:{response_message['realm']}:{options.password}")
-                    # Static noise
-                    ha2 = hex_hash("dummy_method:dummy_uri")
-                    cnonce = secrets.randbelow(10**8)
-                    if "nc" not in response_message:
-                        response_message['nc'] = 1
-                    hashed = hex_hash(f"{ha1}:{response_message['nonce']}:{response_message['nc']}:{cnonce}:auth:{ha2}")
-                    auth = {
-                            "realm": response_message["realm"],
-                            "username": "admin",
-                            "nonce": response_message["nonce"],
-                            "cnonce": cnonce,
-                            "response": hashed,
-                            "algorithm": "SHA-256"
-                    }
-                    self.set_auth(auth)
-                    # Verify auth
-                    try:
-                        await self.call("Sys.GetStatus")
-                    except JSONRPCError as err:
-                        _LOGGER.info("Auth failed, password may be incorrect")
-                        raise err
-                else:
+                if err.code != 401:
                     raise err
+                response_message = json.loads(err.message)
 
+                username = "admin"  # always
+                ha1 = hex_hash(
+                    f"{username}:{response_message['realm']}:{options.password}"
+                )
+                # Static noise
+                ha2 = hex_hash("dummy_method:dummy_uri")
+                cnonce = secrets.randbelow(10**8)
+                if "nc" not in response_message:
+                    response_message["nc"] = 1
+                hashed = hex_hash(
+                    f"{ha1}:{response_message['nonce']}:{response_message['nc']}:{cnonce}:auth:{ha2}"
+                )
+                auth = {
+                    "realm": response_message["realm"],
+                    "username": "admin",
+                    "nonce": response_message["nonce"],
+                    "cnonce": cnonce,
+                    "response": hashed,
+                    "algorithm": "SHA-256",
+                }
+                self.set_auth(auth)
+                # Verify auth
+                try:
+                    await self.call("Sys.GetStatus")
+                except JSONRPCError as err:
+                    _LOGGER.info("Auth failed, password may be incorrect")
+                    raise err
 
     async def disconnect(self) -> None:
         """Disconnect all sessions."""
@@ -252,7 +270,10 @@ class WsRPC:
         """Return if we're currently connected."""
         return self._client is not None and not self._client.closed
 
-    def set_auth(self, auth):
+    def set_auth(self, auth: dict[str, Any]) -> None:
+        """
+        Sets digest auth params
+        """
         self._auth = auth
 
     async def call(
@@ -262,7 +283,12 @@ class WsRPC:
         if self._client is None:
             raise RuntimeError("Not connected")
 
-        call = RPCCall(self._next_id, method, params, self._auth, self._route)
+        if self._auth:
+            if not params:
+                params = {}
+            if "auth" not in params:
+                params["auth"] = self._auth
+        call = RPCCall(self._next_id, method, params, self._route)
         self._calls[call.call_id] = call
         await self._client.send_json(call.request_frame)
 
