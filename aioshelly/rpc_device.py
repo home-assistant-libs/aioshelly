0c6892ee7e13e6df11ca86553ad45799bcd244f9
Shay Levy <levyshay1@gmail.com>
2022-04-11 20:49:28 +0300

    Add Shelly gen2 authentication support (#209)

--- aioshelly/rpc_device.py
+++ aioshelly/rpc_device.py
@@ -88,13 +88,22 @@ class RpcDevice:
         try:
             self.shelly = await get_info(self.aiohttp_session, self.options.ip_address)
 
-            if self.options.auth or not self.requires_auth:
-                await self._wsrpc.connect(self.aiohttp_session)
-                await asyncio.gather(
-                    self.update_device_info(),
-                    self.update_config(),
-                    self.update_status(),
+            if self.requires_auth:
+                if self.options.username is None or self.options.password is None:
+                    raise AuthRequired
+
+                self._wsrpc.set_auth_data(
+                    self.shelly["auth_domain"],
+                    self.options.username,
+                    self.options.password,
                 )
+
+            await self._wsrpc.connect(self.aiohttp_session)
+            await asyncio.gather(
+                self.update_device_info(),
+                self.update_config(),
+                self.update_status(),
+            )
             self.initialized = True
         finally:
             self._initializing = False
--- aioshelly/wsrpc.py
+++ aioshelly/wsrpc.py
@@ -2,7 +2,10 @@
 from __future__ import annotations
 
 import asyncio
+import hashlib
+import json
 import logging
+import time
 from asyncio import tasks
 from dataclasses import dataclass
 from typing import Any, Callable, cast
@@ -16,6 +19,7 @@ from .exceptions import (
     CannotConnect,
     ConnectionClosed,
     ConnectionFailed,
+    InvalidAuthError,
     InvalidMessage,
     JSONRPCError,
     RPCError,
@@ -25,26 +29,71 @@ from .exceptions import (
 _LOGGER = logging.getLogger(__name__)
 
 
+def hex_hash(message: str) -> str:
+    """Get hex representation of sha256 hash of string."""
+    return hashlib.sha256(message.encode("utf-8")).hexdigest()
+
+
+HA2 = hex_hash("dummy_method:dummy_uri")
+
+
 @dataclass
-class RouteData:
-    """RouteData (src/dst) class."""
+class AuthData:
+    """RPC Auth data class."""
+
+    realm: str
+    username: str
+    password: str
+
+    def __post_init__(self) -> None:
+        """Call after initialization."""
+        self.ha1 = hex_hash(f"{self.username}:{self.realm}:{self.password}")
+
+    def get_auth(self, nonce: int | None = None, n_c: int = 1) -> dict[str, Any]:
+        """Get auth for RPC calls."""
+        cnonce = int(time.time())
+        if nonce is None:
+            nonce = cnonce - 1800
+
+        # https://shelly-api-docs.shelly.cloud/gen2/Overview/CommonDeviceTraits/#authentication-over-websocket
+        hashed = hex_hash(f"{self.ha1}:{nonce}:{n_c}:{cnonce}:auth:{HA2}")
+
+        return {
+            "realm": self.realm,
+            "username": self.username,
+            "nonce": nonce,
+            "cnonce": cnonce,
+            "response": hashed,
+            "algorithm": "SHA-256",
+        }
+
+
+@dataclass
+class SessionData:
+    """SessionData (src/dst/auth) class."""
 
     src: str | None
     dst: str | None
+    auth: dict[str, Any] | None
 
 
 class RPCCall:
     """RPCCall class."""
 
     def __init__(
-        self, call_id: int, method: str, params: dict[str, Any] | None, route: RouteData
+        self,
+        call_id: int,
+        method: str,
+        params: dict[str, Any] | None,
+        session: SessionData,
     ):
         """Initialize RPC class."""
+        self.auth = session.auth
         self.call_id = call_id
         self.params = params
         self.method = method
-        self.src = route.src
-        self.dst = route.dst
+        self.src = session.src
+        self.dst = session.dst
         self.resolve: asyncio.Future = asyncio.Future()
 
     @property
@@ -55,7 +104,7 @@ class RPCCall:
             "method": self.method,
             "src": self.src,
         }
-        for obj in ("params", "dst"):
+        for obj in ("params", "dst", "auth"):
             if getattr(self, obj) is not None:
                 msg[obj] = getattr(self, obj)
         return msg
@@ -66,13 +115,14 @@ class WsRPC:
 
     def __init__(self, ip_address: str, on_notification: Callable) -> None:
         """Initialize WsRPC class."""
+        self._auth_data: AuthData | None = None
         self._ip_address = ip_address
         self._on_notification = on_notification
         self._rx_task: tasks.Task[None] | None = None
         self._client: ClientWebSocketResponse | None = None
         self._calls: dict[int, RPCCall] = {}
         self._call_id = 0
-        self._route = RouteData(f"aios-{id(self)}", None)
+        self._session = SessionData(f"aios-{id(self)}", None, None)
 
     @property
     def _next_id(self) -> int:
@@ -107,24 +157,29 @@ class WsRPC:
 
         await self._client.close()
 
+    def set_auth_data(self, realm: str, username: str, password: str) -> None:
+        """Set authentication data and generate session auth."""
+        self._auth_data = AuthData(realm, username, password)
+        self._session.auth = self._auth_data.get_auth()
+
     async def _handle_call(self, frame_id: str) -> None:
         assert self._client
 
         await self._send_json(
             {
                 "id": frame_id,
-                "src": self._route.src,
+                "src": self._session.src,
                 "error": {"code": 500, "message": "Not Implemented"},
             }
         )
 
     def _handle_frame(self, frame: dict[str, Any]) -> None:
         if peer_src := frame.get("src"):
-            if self._route.dst is not None and peer_src != self._route.dst:
+            if self._session.dst is not None and peer_src != self._session.dst:
                 _LOGGER.warning(
-                    "Remote src changed: %s -> %s", self._route.dst, peer_src
+                    "Remote src changed: %s -> %s", self._session.dst, peer_src
                 )
-            self._route.dst = peer_src
+            self._session.dst = peer_src
 
         frame_id = frame.get("id")
 
@@ -205,19 +260,47 @@ class WsRPC:
         return self._client is not None and not self._client.closed
 
     async def call(
-        self, method: str, params: dict[str, Any] | None = None, timeout: int = 10
+        self,
+        method: str,
+        params: dict[str, Any] | None = None,
+        timeout: int = 10,
+        handle_auth: bool = True,
+    ) -> dict[str, Any]:
+        """Websocket RPC call."""
+        # Try request with initial/last call auth data
+        resp = await self._rpc_call(method, params, timeout)
+        if "result" in resp:
+            return cast(dict, resp["result"])
+
+        try:
+            code, msg = resp["error"]["code"], resp["error"]["message"]
+        except KeyError as err:
+            raise RPCError(f"bad response: {resp}") from err
+
+        if code != 401:
+            raise JSONRPCError(code, msg)
+        if not handle_auth or self._auth_data is None:
+            raise InvalidAuthError(code, msg)
+
+        # Update auth from response and try with new auth data
+        auth = json.loads(msg)
+        self._session.auth = self._auth_data.get_auth(auth["nonce"], auth.get("nc", 1))
+        return await self.call(method, params, timeout, handle_auth=False)
+
+    async def _rpc_call(
+        self, method: str, params: dict[str, Any] | None, timeout: int
     ) -> dict[str, Any]:
         """Websocket RPC call."""
         if self._client is None:
             raise RuntimeError("Not connected")
 
-        call = RPCCall(self._next_id, method, params, self._route)
+        call = RPCCall(self._next_id, method, params, self._session)
         self._calls[call.call_id] = call
         await self._send_json(call.request_frame)
 
         try:
             async with async_timeout.timeout(timeout):
-                resp = await call.resolve
+                resp: dict[str, Any] = await call.resolve
         except asyncio.TimeoutError as exc:
             _LOGGER.warning("%s timed out: %s", call, exc)
             raise RPCTimeout(call) from exc
@@ -225,15 +308,8 @@ class WsRPC:
             _LOGGER.error("%s ???: %s", call, exc)
             raise RPCError(call, exc) from exc
 
-        if "result" in resp:
-            _LOGGER.debug("%s(%s) -> %s", call.method, call.params, resp["result"])
-            return cast(dict, resp["result"])
-
-        try:
-            code, msg = resp["error"]["code"], resp["error"]["message"]
-            raise JSONRPCError(code, msg)
-        except KeyError as err:
-            raise RPCError(f"bad response: {resp}") from err
+        _LOGGER.debug("%s(%s) -> %s", call.method, call.params, resp)
+        return resp
 
     async def _send_json(self, data: dict[str, Any]) -> None:
         """Send json frame to device."""
