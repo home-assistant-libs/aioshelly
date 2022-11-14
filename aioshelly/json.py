1693cc45d2b939b76d0291a2daaef7a04e5c4fb0
J. Nick Koston <nick@koston.org>
2022-11-14 11:55:50 -0600

    Improve JSON performance (#282)

---
+++ aioshelly/json.py
@@ -0,0 +1,23 @@
+"""JSON helper."""
+
+from typing import Any
+
+import orjson
+
+json_loads = orjson.loads  # pylint: disable=no-member
+
+
+def json_encoder_default(obj: Any) -> Any:
+    """Convert objects."""
+    if isinstance(obj, (set, tuple)):
+        return list(obj)
+    raise TypeError
+
+
+def json_dumps(data: Any) -> str:
+    """Dump json string."""
+    return orjson.dumps(  # pylint: disable=no-member
+        data,
+        option=orjson.OPT_NON_STR_KEYS,  # pylint: disable=no-member
+        default=json_encoder_default,
+    ).decode("utf-8")
