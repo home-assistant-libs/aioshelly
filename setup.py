6bd6b5ce803e442ce6620f2a7be7805f221083ba
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-11-25 12:00:11 +0100

    Add Python 3.12 to CI

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
