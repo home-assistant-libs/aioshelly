e0fd31112d2432232f0f8ca879a062cdd600ac09
taulfsime <k.todorov@allterco.com>
2024-01-10 09:12:39 +0200

    Bring back scan options

--- aioshelly/ble/const.py
+++ aioshelly/ble/const.py
@@ -5,11 +5,18 @@ BLE_SCAN_RESULT_EVENT = "ble.scan_result"
 
 BLE_SCRIPT_NAME = "aioshelly_ble_integration"
 
-BLE_SCAN_RESULT_VERSION = 1
+BLE_SCAN_RESULT_VERSION = 2
 
 VAR_EVENT_TYPE = "%event_type%"
 VAR_ACTIVE = "%active%"
 VAR_VERSION = "%version%"
+VAR_INTERVAL_MS = "%interval_ms%"
+VAR_WINDOW_MS = "%window_ms%"
+VAR_DURATION_MS = "%duration_ms%"
+
+DEFAULT_INTERVAL_MS = 320
+DEFAULT_WINDOW_MS = 30
+DEFAULT_DURATION_MS = -1
 
 BLE_CODE = """
 // aioshelly BLE script 2.0
@@ -63,8 +70,10 @@ function bleCallback(event, res) {
 // Skip starting if scanner is active
 if (!BLE.Scanner.isRunning()) {
   BLE.Scanner.Start({
-    duration_ms: -1,
+    duration_ms: %duration_ms%,
     active: %active%,
+    interval_ms: %interval_ms%,
+    window_ms: %window_ms%,
   });
 }
 
