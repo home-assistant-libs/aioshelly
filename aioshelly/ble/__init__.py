ccc6ac69c6324502b5dbbb3cf910cce324b27f2d
J. Nick Koston <nick@koston.org>
2024-01-12 12:47:15 -1000

    fix parser

--- aioshelly/ble/__init__.py
+++ aioshelly/ble/__init__.py
@@ -11,7 +11,6 @@ from ..exceptions import RpcCallError
 from ..rpc_device import RpcDevice
 from .const import (
     BLE_CODE,
-    BLE_SCAN_RESULT_VERSION,
     BLE_SCRIPT_NAME,
     VAR_ACTIVE,
     VAR_DURATION_MS,
@@ -84,33 +83,35 @@ async def async_start_scanner(  # pylint: disable=too-many-arguments
 
 
 def parse_ble_scan_result_event(
-    data: dict[str, Any],
-) -> tuple[str, int, BLEGAPAdvertisement]:
+    data: list[Any],
+) -> list[tuple[str, int, BLEGAPAdvertisement]]:
     """Parse BLE scan result event."""
-    version: int = data.ver
-    if version != BLE_SCAN_RESULT_VERSION:
-        raise ValueError(f"Unsupported BLE scan result version: {version}")
+    version: int = data[0]
+    if version == 1:
+        return [_adv_to_ble_tuple(data[1])]
+    elif version == 2:
+        advs: list[Any] = data[1]
+        return [_adv_to_ble_tuple(adv) for adv in advs]
+    raise ValueError(f"Unsupported BLE scan result version: {version}")
 
-    result: list[Any] = []
 
-    for adv in data.advs:
-        address: str = adv[0]
-        rssi: int = adv[1]
-        advertisement_data_b64: str = adv[2]
-        scan_response_b64: str = adv[3]
-
-        result.append({
-            "address": address.upper(),
-            "rssi": rssi,
-            "adv": parse_advertisement_data(
-                (
-                    a2b_base64(advertisement_data_b64.encode("ascii")),
-                    a2b_base64(scan_response_b64.encode("ascii")),
-                ),
-            ),
-        })
-
-    return result
+def _adv_to_ble_tuple(adv: list[Any]) -> tuple[str, int, BLEGAPAdvertisement]:
+    """Convert adv a ble tuple."""
+    address: str
+    rssi: int
+    advertisement_data_b64: str
+    scan_response_b64: str
+    address, rssi, advertisement_data_b64, scan_response_b64 = adv
+    return (
+        address.upper(),
+        rssi,
+        parse_advertisement_data(
+            (
+                a2b_base64(advertisement_data_b64.encode("ascii")),
+                a2b_base64(scan_response_b64.encode("ascii")),
+            )
+        ),
+    )
 
 
 async def async_ensure_ble_enabled(device: RpcDevice) -> bool:
