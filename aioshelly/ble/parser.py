1b8e18bd27afad36e83f727353b4ef675d3867c8
J. Nick Koston <nick@koston.org>
2024-01-12 13:32:33 -1000

    fix v1 legacy

--- aioshelly/ble/parser.py
+++ aioshelly/ble/parser.py
@@ -20,27 +20,43 @@ def parse_ble_scan_result_event(
     """Parse BLE scan result event."""
     version: int = data[0]
     if version == 1:
-        return [_adv_to_ble_tuple(data[1])]
+        return _parse_v1(data)
     if version == 2:
-        advs: list[Any] = data[1]
-        return [_adv_to_ble_tuple(adv) for adv in advs]
+        return _parse_v2(data[1])
     raise ValueError(f"Unsupported BLE scan result version: {version}")
 
 
-def _adv_to_ble_tuple(adv: list[Any]) -> tuple[str, int, BLEGAPAdvertisementTupleType]:
-    """Convert adv a ble tuple."""
-    address: str
-    rssi: int
-    advertisement_data_b64: str
-    scan_response_b64: str
-    address, rssi, advertisement_data_b64, scan_response_b64 = adv
-    return (
-        address.upper(),
-        rssi,
-        parse_advertisement_data_tuple(
-            (
-                a2b_base64(advertisement_data_b64.encode("ascii")),
-                a2b_base64(scan_response_b64.encode("ascii")),
-            )
-        ),
-    )
+def _parse_v1(adv: list[Any]) -> list[tuple[str, int, BLEGAPAdvertisementTupleType]]:
+    """Convert v1 format to a list of ble tuples."""
+    _, address, rssi, advertisement_data_b64, scan_response_b64 = adv
+    return [
+        (
+            address.upper(),
+            rssi,
+            parse_advertisement_data_tuple(
+                (
+                    a2b_base64(advertisement_data_b64.encode("ascii")),
+                    a2b_base64(scan_response_b64.encode("ascii")),
+                )
+            ),
+        )
+    ]
+
+
+def _parse_v2(
+    advs: list[list[Any]],
+) -> list[tuple[str, int, BLEGAPAdvertisementTupleType]]:
+    """Convert v2 format to a list of ble tuples."""
+    return [
+        (
+            address.upper(),
+            rssi,
+            parse_advertisement_data_tuple(
+                (
+                    a2b_base64(advertisement_data_b64.encode("ascii")),
+                    a2b_base64(scan_response_b64.encode("ascii")),
+                )
+            ),
+        )
+        for address, rssi, advertisement_data_b64, scan_response_b64 in advs
+    ]
