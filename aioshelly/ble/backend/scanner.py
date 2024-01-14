feb1a50aa94f8f0901dbb8809e24b0367945e2f4
taulfsime <34596275+taulfsime@users.noreply.github.com>
2024-01-14 20:28:00 +0200

    Bluetooth scanner version 2 (#478)
    Co-authored-by: taulfsime <k.todorov@allterco.com>
    Co-authored-by: J. Nick Koston <nick@koston.org>

---
+++ aioshelly/ble/backend/scanner.py
@@ -0,0 +1,40 @@
+"""Bluetooth scanner for shelly."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from bluetooth_data_tools import monotonic_time_coarse
+from habluetooth import BaseHaRemoteScanner
+
+from ..const import BLE_SCAN_RESULT_EVENT
+from ..parser import parse_ble_scan_result_event
+
+LOGGER = logging.getLogger(__name__)
+
+
+class ShellyBLEScanner(BaseHaRemoteScanner):
+    """Scanner for shelly."""
+
+    def async_on_event(self, event: dict[str, Any]) -> None:
+        """Process an event from the shelly and ignore if its not a ble.scan_result."""
+        if event.get("event") != BLE_SCAN_RESULT_EVENT:
+            return
+
+        try:
+            parsed_advs = parse_ble_scan_result_event(event["data"])
+        except Exception as err:  # pylint: disable=broad-except
+            # Broad exception catch because we have no
+            # control over the data that is coming in.
+            LOGGER.error("Failed to parse BLE event: %s", err, exc_info=True)
+            return
+
+        now = monotonic_time_coarse()
+        for address, rssi, parsed in parsed_advs:
+            self._async_on_advertisement(
+                address,
+                rssi,
+                *parsed,
+                {},
+                now,
+            )
--- aioshelly/ble/const.py
+++ aioshelly/ble/const.py
@@ -1,11 +1,11 @@
-"""Shelly Gen2 BLE support."""
+"""Shelly Gen2+ BLE support."""
 from __future__ import annotations
 
 BLE_SCAN_RESULT_EVENT = "ble.scan_result"
 
 BLE_SCRIPT_NAME = "aioshelly_ble_integration"
 
-BLE_SCAN_RESULT_VERSION = 1
+BLE_SCAN_RESULT_VERSION = 2
 
 VAR_EVENT_TYPE = "%event_type%"
 VAR_ACTIVE = "%active%"
@@ -19,22 +19,66 @@ DEFAULT_WINDOW_MS = 30
 DEFAULT_DURATION_MS = -1
 
 BLE_CODE = """
-// aioshelly BLE script 1.0
-BLE.Scanner.Subscribe(function (ev, res) {
-    if (ev === BLE.Scanner.SCAN_RESULT) {
-        Shelly.emitEvent("%event_type%", [
-            %version%,
-            res.addr,
-            res.rssi,
-            btoa(res.advData),
-            btoa(res.scanRsp)
-        ]);
+// aioshelly BLE script 2.0
+const queueServeTimer = 100; // in ms, timer for events emitting
+const burstSendCount =  5; // number if events, emitted on timer event
+const maxQueue =  80; // if the queue exceeds the limit, all new events are ignored until it empties
+const packetsInSingleEvent = 16; // max number of packets in single event
+
+let queue = [];
+let timerHandler = null;
+
+function timerCallback() {
+  for(let i = 0; i < burstSendCount; i++) {
+    if (queue.length <= 0) {
+      break;
     }
-});
-BLE.Scanner.Start({
+
+    Shelly.emitEvent(
+      "%event_type%", [
+        %version%,
+        queue.slice(0, packetsInSingleEvent),
+      ]
+    );
+    queue = queue.slice(packetsInSingleEvent);
+  }
+
+  timerHandler = null;
+  if (queue.length > 0) {
+    timerHandler = Timer.set(queueServeTimer, false, timerCallback);
+  }
+}
+
+function bleCallback(event, res) {
+  if (event !== BLE.Scanner.SCAN_RESULT) {
+    return
+  }
+
+  if (queue.length > maxQueue) {
+    return;
+  }
+
+  queue.push([
+    res.addr,
+    res.rssi,
+    btoa(res.advData),
+    btoa(res.scanRsp)
+  ]);
+
+  if(!timerHandler) {
+    timerHandler = Timer.set(queueServeTimer, false, timerCallback);
+  }
+}
+
+// Skip starting if scanner is active
+if (!BLE.Scanner.isRunning()) {
+  BLE.Scanner.Start({
     duration_ms: %duration_ms%,
     active: %active%,
     interval_ms: %interval_ms%,
     window_ms: %window_ms%,
-});
+  });
+}
+
+BLE.Scanner.Subscribe(bleCallback);
 """
