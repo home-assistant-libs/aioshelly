c339d6861856afa590f6baa8fada8f254c8a695e
Shay Levy <levyshay1@gmail.com>
2024-09-02 21:39:32 +0300

    Add tests for process_ip_or_options

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,8 +1,15 @@
 """Tests for common module."""
 
-import pytest
+from unittest.mock import patch
 
-from aioshelly.common import is_firmware_supported
+import pytest
+from aiohttp import BasicAuth
+
+from aioshelly.common import (
+    ConnectionOptions,
+    is_firmware_supported,
+    process_ip_or_options,
+)
 
 
 @pytest.mark.parametrize(
@@ -21,3 +28,26 @@ def test_is_firmware_supported(
 ) -> None:
     """Test is_firmware_supported function."""
     assert is_firmware_supported(gen, model, firmware_version) is expected
+
+
+@pytest.mark.asyncio
+async def test_process_ip_or_options():
+    """Test process_ip_or_options function."""
+    ip = "192.168.20.11"
+
+    # Test string numeric IP address
+    assert await process_ip_or_options(ip) == ConnectionOptions(ip)
+
+    # Test string hostname IP address
+    with patch("aioshelly.common.gethostbyname", return_value=ip):
+        assert await process_ip_or_options("some_host") == ConnectionOptions(ip)
+
+    # Test ConnectionOptions
+    options = ConnectionOptions(ip, "user", "pass")
+    assert await process_ip_or_options(options) == options
+    assert options.auth == BasicAuth("user", "pass")
+
+    # Test missing password
+    with pytest.raises(ValueError):
+        options = ConnectionOptions(ip, "user")
+        await process_ip_or_options(options)
