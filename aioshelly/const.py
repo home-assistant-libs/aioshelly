d4bc23498c205954b8af326cb2f497725e8723aa
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-10-20 11:38:31 +0200

    Add bthomedevice to VIRTUAL_COMPONENTS

--- aioshelly/const.py
+++ aioshelly/const.py
@@ -973,6 +973,14 @@ END_OF_OPTIONS_MARKER = 0xFF
 
 FIRMWARE_PATTERN = re.compile(r"^(\d{8})")
 
-VIRTUAL_COMPONENTS = {"blutrv", "boolean", "button", "enum", "number", "text"}
+VIRTUAL_COMPONENTS = {
+    "blutrv",
+    "boolean",
+    "bthomedevice",
+    "button",
+    "enum",
+    "number",
+    "text",
+}
 # Firmware 1.2.0 release date
 VIRTUAL_COMPONENTS_MIN_FIRMWARE = 20240213
