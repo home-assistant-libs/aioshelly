de4b5638d2fc81817280500321d30467123ff8da
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-11-25 08:34:01 +0100

    Meet ruff 0.8.0 requirements

--- aioshelly/block_device/__init__.py
+++ aioshelly/block_device/__init__.py
@@ -3,4 +3,4 @@
 from .coap import COAP
 from .device import BLOCK_VALUE_UNIT, Block, BlockDevice, BlockUpdateType
 
-__all__ = ["COAP", "BLOCK_VALUE_UNIT", "Block", "BlockDevice", "BlockUpdateType"]
+__all__ = ["BLOCK_VALUE_UNIT", "COAP", "Block", "BlockDevice", "BlockUpdateType"]
