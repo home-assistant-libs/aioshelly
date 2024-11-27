e2350e399f1c0e55308184cccae605746f10d624
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-11-27 12:32:01 +0100

    Restore use of `find_packages` in setup.py (#725)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
@@ -21,7 +21,7 @@ setup(
     description="Asynchronous library to control Shelly devices.",
     long_description=README_FILE.read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    packages=["aioshelly"],
+    packages=find_packages(exclude=["tests", "tools"]),
     python_requires=">=3.11",
     package_data={"aioshelly": ["py.typed"]},
     zip_safe=True,
