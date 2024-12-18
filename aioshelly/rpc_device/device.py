2c99523dcc51cd076f39a04010d5b387df951849
Simone Chemelli <simone.chemelli@gmail.com>
2024-12-18 08:02:32 +0000

    attrs not always available

--- aioshelly/rpc_device/device.py
+++ aioshelly/rpc_device/device.py
@@ -565,13 +565,14 @@ class RpcDevice:
 
                 cfg: dict[str, Any] = results[0]["config"]["trv:0"]
                 # addr, name and model_id must be added from Shelly.GetComponents call
+                _attrs = component.get("attrs")
                 cfg.update({"addr": component["config"]["addr"]})
                 cfg.update({"name": component["config"]["name"]})
                 cfg.update(
                     {
-                        "model_id": BLU_TRV_MODEL_ID.get(
-                            component["attrs"].get("model_id")
-                        )
+                        "model_id": BLU_TRV_MODEL_ID.get(_attrs.get("model_id"))
+                        if _attrs
+                        else None
                     }
                 )
                 self._config.update({component["key"]: cfg})
