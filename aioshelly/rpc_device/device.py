401ca57f42d15d6845efe11d7dd548d36d77ed0f
Simone Chemelli <simone.chemelli@gmail.com>
2025-02-03 21:51:11 +0000

    return type

--- aioshelly/rpc_device/device.py
+++ aioshelly/rpc_device/device.py
@@ -501,7 +501,7 @@ class RpcDevice:
         return cast(str, self.shelly["model"])
 
     @property
-    def xmod_info(self) -> dict:
+    def xmod_info(self) -> dict[str, Any]:
         """Device XMOD properties."""
         return cast(dict, self.shelly.get("jwt", {}))
 
