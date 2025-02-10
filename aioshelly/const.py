96f5d16c2a178aade21b14463b941d2b31e3590d
Simone Chemelli <simone.chemelli@gmail.com>
2025-02-10 20:46:26 +0000

    Models exception list for BT Proxy support

--- aioshelly/const.py
+++ aioshelly/const.py
@@ -1019,6 +1019,8 @@ UNDEFINED = UndefinedType._singleton  # noqa: SLF001
 
 MODEL_NAMES = {data.model: data.name for data in DEVICES.values()}
 
+MODELS_NOT_SUPPORTING_BT_PROXY = [MODEL_WALL_DISPLAY]
+
 # Timeout used for Device IO
 DEVICE_IO_TIMEOUT = 10.0
 
