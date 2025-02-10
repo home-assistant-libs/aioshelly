1a06e949460d8399e05edf562e99c3d7b8f099ad
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-02-10 23:27:42 +0100

    Add test_retrieve_blutrv_components

--- tests/rpc_device/test_device.py
+++ tests/rpc_device/test_device.py
@@ -1,10 +1,12 @@
 """Tests for rpc_device.device module."""
 
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 
 from aioshelly.rpc_device.device import RpcDevice, mergedicts
+
+from . import load_device_fixture
 
 VIRT_COMP_STATUS = {"value": 0}
 VIRT_COMP_CONFIG = {
@@ -72,3 +74,39 @@ async def test_parse_dynamic_components_with_attrs() -> None:
 
     assert device._status["number:200"] == VIRT_COMP_STATUS
     assert device._config["number:200"] == {**VIRT_COMP_CONFIG, **VIRT_COMP_ATTRS}
+
+
+@pytest.mark.asyncio
+async def test_retrieve_blutrv_components() -> None:
+    """Test _retrieve_blutrv_components() method."""
+    device = await RpcDevice.create(Mock(), Mock(), "10.10.10.10")
+
+    device._config = await load_device_fixture(
+        "shellyblugatewaygen3", "Shelly.GetConfig"
+    )
+    device._status = await load_device_fixture(
+        "shellyblugatewaygen3", "Shelly.GetStatus"
+    )
+    device._shelly = await load_device_fixture("shellyblugatewaygen3", "shelly.json")
+    device.initialized = True
+
+    components = await load_device_fixture(
+        "shellyblugatewaygen3", "Shelly.GetComponents"
+    )
+    remote_config = await load_device_fixture(
+        "shellyblugatewaygen3", "BluTrv.GetRemoteConfig"
+    )
+    device.call_rpc = AsyncMock(return_value=remote_config)
+
+    await device._retrieve_blutrv_components(components)
+
+    assert device.config["blutrv:200"]["local_name"] == "SBTR-001AEU"
+    assert device.config["blutrv:200"]["name"] == "Shelly BLU TRV [DDEEFF]"
+    assert device.config["blutrv:200"]["addr"] == "aa:bb:cc:dd:ee:ff"
+    assert device.config["blutrv:200"]["enable"] is True
+
+    assert device.status["blutrv:200"]["current_C"] == 21
+    assert device.status["blutrv:200"]["target_C"] == 19
+    assert device.status["blutrv:200"]["pos"] == 0
+    assert device.status["blutrv:200"]["rssi"] == -58
+    assert device.status["blutrv:200"]["errors"] == []
