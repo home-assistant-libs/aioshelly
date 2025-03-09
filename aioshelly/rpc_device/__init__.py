f8f19fc64aae763096b9a15adef01c6b34821e59
J. Nick Koston <nick@koston.org>
2025-03-08 17:08:36 -1000

    Add support for getting the bluetooth MAC form the primary MAC

--- aioshelly/rpc_device/__init__.py
+++ aioshelly/rpc_device/__init__.py
@@ -1,6 +1,7 @@
 """Shelly Gen2 RPC based device."""
 
 from .device import RpcDevice, RpcUpdateType
+from .utils import bluetooth_mac_from_primary_mac
 from .wsrpc import WsServer
 
-__all__ = ["RpcDevice", "RpcUpdateType", "WsServer"]
+__all__ = ["RpcDevice", "RpcUpdateType", "WsServer", "bluetooth_mac_from_primary_mac"]
