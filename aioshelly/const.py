0ed742f85ef1b5241f2d1dae675edf2a630276eb
J. Nick Koston <nick@koston.org>
2025-11-27 16:03:38 -0600

    Add BLE model_id for Plus RGBW PM and Plus 0-10V Dimmer (#1003)

--- aioshelly/const.py
+++ aioshelly/const.py
@@ -547,6 +547,7 @@ DEVICES = {
         min_fw_date=GEN2_MIN_FIRMWARE_DATE,
         gen=GEN2,
         supported=True,
+        model_id=0x1070,
     ),
     MODEL_PLUS_1PM: ShellyDevice(
         model=MODEL_PLUS_1PM,
@@ -660,6 +661,7 @@ DEVICES = {
         min_fw_date=GEN2_MIN_FIRMWARE_DATE,
         gen=GEN2,
         supported=True,
+        model_id=0x1009,
     ),
     MODEL_PLUS_SMOKE: ShellyDevice(
         model=MODEL_PLUS_SMOKE,
