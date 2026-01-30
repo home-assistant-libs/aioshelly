fe3d3db573ca98f19a6538913cf47e24a6e91c03
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-01-30 15:44:41 +0000

    Editable mode

--- script/environment.sh
+++ script/environment.sh
@@ -3,5 +3,5 @@
 pip3 --disable-pip-version-check --no-cache-dir install uv
 uv venv venv --seed --clear
 source venv/bin/activate
-uv pip install --no-cache-dir .[dev] .[lint]
+uv pip install --no-cache-dir -e .[dev] .[lint]
 prek install
