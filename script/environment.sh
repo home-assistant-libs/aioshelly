04046f0fd0748bf63f7dcc53cd0acf83c4a22345
Simone Chemelli <simone.chemelli@gmail.com>
2026-05-10 14:33:03 +0000

    Fix directory typo in build script

--- script/environment.sh
+++ script/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip3 --disable-pip-version-check --no-cache-dir install uv
-uv venv venv --seed --clear
-source venv/bin/activate
+uv venv .venv --seed --clear
+source .venv/bin/activate
 uv pip install --no-cache-dir -e .[dev] .[lint]
 prek install
