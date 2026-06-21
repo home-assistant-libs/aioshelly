8bd519dcdecef1e56ded15b087693b5795227306
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-06-21 09:10:04 +0000

    Fix uv warning

--- script/environment.sh
+++ script/environment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use copy mode for UV to avoid hardlink warnings on different filesystems
+export UV_LINK_MODE=copy
+
 pip3 --disable-pip-version-check --no-cache-dir install uv
 uv venv .venv --seed --clear
 source .venv/bin/activate
